09abbf916aafd9b5b8f9fe9ebc4ac8b793617b64
franco <delgadofranco992@gmail.com>
2024-12-28 04:04:44 -0300

    28/12
    conexion de cliente con admin (MUESTRA)

--- cafe-bar/cliente/principal.js
+++ cafe-bar/cliente/principal.js
@@ -2,21 +2,15 @@ alert("correct")
 /*ADMINISTRADOR*/
 let cantidad = document.querySelector(".cantidad")
 
-
-
-
 /*CODIGGO BEBIDAS*/
 let contenedorMuestra = document.querySelector(".contenedorMuestra");
 let inputMuestra = document.querySelector(".inputMuestra");
 let manto = document.querySelector(".manto");
 let contenedor = document.querySelector(".contenedor");
-var input = document.querySelector(".input")
+var input = document.querySelector(".input");
 let cantidadPedido = null;
+let pedido = "";
 
-function listo() {
-  window.location.href = "../administrador"
-  cantidad.innerHTML = inputMuestra;
-}
 function cancelar() {
   input.value = null;
   manto.style.display = "none";
@@ -25,17 +19,31 @@ function cancelar() {
 function habilitar1() {
   manto.style.display = "grid";
   contenedor.style.display = "grid";
+  pedido = "pedido 1";
 }
 function habilitar2() {
   manto.style.display = "grid";
   contenedor.style.display = "grid";
+  pedido = "pedido 2";
 }
+//inputMuestra
 function cargar() {
   cantidadPedido = input.value;
   input.value = null;
-  alert(" cantidad: " + cantidadPedido);
+  alert(pedido+" Cantidad: " + cantidadPedido);
   manto.style.display = "none";
   contenedor.style.display = "none";
   contenedorMuestra.style.display = "grid";
-  inputMuestra.innerHTML = cantidadPedido
+  inputMuestra.innerHTML = cantidadPedido;
+}
+
+
+function listo() {
+
+  // Guardar los datos en localStorage
+  localStorage.setItem('nombre', pedido);
+  localStorage.setItem('cantidad', cantidadPedido);
+
+  console.log("Datos guardados en localStorage");
+  window.location.href = "../administrador"
 }
